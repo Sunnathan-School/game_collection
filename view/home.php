7092d23661e55d5a448abccfe129efc2cc636f9a
Nathan Sun <nathan.sunyt@gmail.com>
2023-12-15 12:07:03 +0100

    change assets path

--- view/home.php
+++ view/home.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/style/home.css">
+    <link rel="stylesheet" href="assets/style/home.css">
     <title>Accueil</title>
 </head>
 <body>
     <div class="home_header">
         <h2>Salut {USERNAME} !</h2>
         <h2>Prêt à ajouter des jeux à ta collection ?</h2>
-        <img src="../assets/img/zelda_home.jpeg" alt="">
+        <img src="assets/img/zelda_home.jpeg" alt="">
         
 
     </div>
