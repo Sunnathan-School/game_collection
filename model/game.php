836864c39e2107d34a8d964686b81b47e3578136
Nathan Sun <nathan.sunyt@gmail.com>
2023-12-15 11:41:02 +0100

    feat: initiate model, view, controller file

---
+++ model/game.php
@@ -0,0 +1,48 @@
+<?php
+
+
+/**
+ * Ajoute un jeu
+ *
+ * @param  string $gameName Nom du jeu
+ * @param  string $gameDesc Description du jeu
+ * @param  string $gameEditor Editeur du jeur
+ * @param  date $gameRelease Date de sortie du jeu
+ * @param  string $gameCoverUrl Url de la couverture du jeu
+ * @param  string $gameWebUrl Url du site web du jeu 
+ * @param  array $platform Liste des plateforms du jeu
+ * @return void
+ */
+function addGame($gameName, $gameDesc, $gameEditor, $gameRelease, $gameCoverUrl, $gameWebUrl,$platform){
+    
+}
+
+/**
+ * Supprime un jeu
+ *
+ * @param  integer $gameId
+ * @return void
+ */
+function removeGame($gameId){}
+
+/**
+ * Modifie un jeu
+ *
+ * @param  mixed $gameId Identifiant du jeu
+ * @param  string $gameName Nom du jeu
+ * @param  string $gameDesc Description du jeu
+ * @param  string $gameEditor Editeur du jeur
+ * @param  date $gameRelease Date de sortie du jeu
+ * @param  string $gameCoverUrl Url de la couverture du jeu
+ * @param  string $gameWebUrl Url du site web du jeu 
+ * @param  array $platform Liste des plateforms du jeu
+ * @return void
+ */
+function editGame($gameId, $gameName, $gameDesc, $gameEditor, $gameRelease, $gameCoverUrl, $gameWebUrl,$platform){}
+
+/**
+ * Récupère tous les jeux
+ *
+ * @return void
+ */
+function getGames(){}
