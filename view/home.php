ae6d5dc3af00b96fc8495cd5bdd8db56f7269731
Nathan Sun <nathan.sunyt@gmail.com>
2023-12-15 10:36:18 +0100

    home

---
+++ view/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <div class="home_header">
+        <h2>Salut {USERNAME} !</h2>
+        <h2>Prêt à ajouter des jeux à ta collection ?</h2>
+        <img src="../assets/img/cover_home.png" alt="">
+
+
+    </div>
+</body>
+</html>
