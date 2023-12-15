951cdb9617f3ee46fe8c38a8b9e8202bc122cafc
Vigouroux Johanne <102874093+johannvig@users.noreply.github.com>
2023-12-15 11:15:48 +0100

    update BDD

--- script.sql
+++ script.sql
@@ -1,3 +1,5 @@
+-- Partie création de tables --
+
 CREATE TABLE CLIENT(
    Id_client INT AUTO_INCREMENT,
    prenom_client VARCHAR(50) NOT NULL,
@@ -44,3 +46,47 @@ CREATE TABLE appartenir(
    FOREIGN KEY(Id_jeux) REFERENCES JEUX(Id_jeux),
    FOREIGN KEY(Id_plateforme) REFERENCES PLATEFORME(Id_plateforme)
 );
+
+
+
+-- Partie jeux de données --
+
+
+INSERT INTO CLIENT (prenom_client, email_client, mdp_client, nom_client) VALUES
+('Alex', 'alex@example.com', 'password123', 'Durand'),
+('Mila', 'mila@example.com', 'password456', 'Dupont'),
+('Lucas', 'lucas@example.com', 'password789', 'Martin');
+
+
+INSERT INTO JEUX (nom_jeux, description_jeux, editeur_jeux, date_sortie_jeux, plateforme_jeux, couverture_url_jeux, _site_url_jeux) VALUES
+('The Witcher 3: Wild Hunt', 'An open-world RPG set in a gritty fantasy universe', 'CD Projekt', '2015-05-18', 'PC', 'https://th.bing.com/th/id/OIP.ESLUrniRRUtzAk-LMGNjAAAAAA?rs=1&pid=ImgDetMain', 'https://thewitcher.com'),
+('Red Dead Redemption 2', 'An epic tale of life in America at the dawn of the modern age', 'Rockstar Games', '2018-10-26', 'PlayStation', 'https://th.bing.com/th/id/OIP.ecoMHvpEDcaJGEHooAQE4QAAAA?rs=1&pid=ImgDetMain', 'https://www.rockstargames.com/reddeadredemption2/'),
+('Super Mario Odyssey', 'Mario embarks on a new journey through unknown worlds', 'Nintendo', '2017-10-27', 'Nintendo', 'https://th.bing.com/th/id/R.fd3644a9d9258f4cea239d99df14af48?rik=p3dc0OPX9KLzKw&riu=http%3a%2f%2fimages.nintendolife.com%2fnews%2f2017%2f10%2fsuper_mario_odyssey_has_already_cleared_2_million_sales%2fattachment%2f0%2foriginal.jpg&ehk=cABXnhtGW9LOLL%2bYXYbVZJ31MZQ5%2bYH%2f5%2fpuNtvCh5w%3d&risl=&pid=ImgRaw&r=0', 'https://www.nintendo.com/games/detail/super-mario-odyssey-switch/'),
+('Halo Infinite', 'The next chapter of the legendary Halo series', '343 Industries', '2021-12-08', 'Xbox', 'https://th.bing.com/th/id/R.b88688beade9d2a55595c44a94d4051d?rik=qNwmqZp8c4mgUg&pid=ImgRaw&r=0', 'https://www.halowaypoint.com/en-us/games/halo-infinite'),
+('Cyberpunk 2077', 'An open-world, action-adventure story set in Night City', 'CD Projekt', '2020-12-10', 'PC', 'https://th.bing.com/th/id/R.e70e98eee8f8f2eb33ca0ac0e7469e0a?rik=dlIDk%2bs642X81Q&pid=ImgRaw&r=0', 'https://www.cyberpunk.net');
+
+
+
+INSERT INTO PLATEFORME (nom_plateforme) VALUES
+('PC'),
+('Nintendo'),
+('Mobile'),
+('Xbox'),
+('PlayStation');
+
+
+INSERT INTO collectionner (Id_client, Id_jeux, heures_jouees_collection, date_ajout_collection) VALUES
+(1, 1, 120, '2021-04-05'),
+(2, 2, 85, '2022-08-16'),
+(3, 3, 50, '2023-02-10'),
+(4, 4, 30, '2021-12-15'),
+(5, 5, 200, '2020-12-17');
+
+
+
+INSERT INTO appartenir (Id_jeux, Id_plateforme, Num_odre_plateforme) VALUES
+(1, 1, 1),
+(2, 5, 1),
+(3, 2, 1),
+(4, 4, 1),
+(5, 1, 2);
