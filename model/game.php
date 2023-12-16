2b23ef575766c9d692c90f6b1d9776dedab53522
Vigouroux Johanne <102874093+johannvig@users.noreply.github.com>
2023-12-16 11:01:59 +0100

    update all game
    -mise en place de la sécurisation (xss, sql, CSRF)

--- model/game.php
+++ model/game.php
@@ -1,5 +1,51 @@
 <?php
 
+// Vérifiez le jeton CSRF
+session_start();
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+
+// Connexion à la base de donnée partira par la suite
+
+
+$host = 'localhost';
+$db = 'jeux'; // This is your database name
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+
+//vérifier que le POST a été fait avant d'ajouter un jeu
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_game']) && hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
+
+    $gameName = $_POST['game_name'] ?? '';
+    $gameDesc = $_POST['game_desc'] ?? '';
+    $gameEditor = $_POST['game_editor'] ?? '';
+    $gameRelease = $_POST['game_release'] ?? '';
+    $gameCoverUrl = $_POST['game_cover_url'] ?? '';
+    $gameWebUrl = $_POST['game_web_url'] ?? '';
+    $platforms = $_POST['platforms'] ?? []; // Permet d'avoir plusieurs cases à cocher en metant un tbaleau
+
+    // Appeler votre fonction pour ajouter le jeu dans la BDD
+    addGame($pdo, $gameName, $gameDesc, $gameEditor, $gameRelease, $gameCoverUrl, $gameWebUrl, $platforms);
+}
+
 
 /**
  * Ajoute un jeu
@@ -13,7 +59,15 @@
  * @param  array $platform Liste des plateforms du jeu
  * @return void
  */
+
+
 function addGame($pdo, $gameName, $gameDesc, $gameEditor, $gameRelease, $gameCoverUrl, $gameWebUrl, $platforms) {
+
+    $gameName = filter_var($gameName, FILTER_SANITIZE_STRING);
+    $gameDesc = filter_var($gameDesc, FILTER_SANITIZE_STRING);
+    $gameEditor = filter_var($gameEditor, FILTER_SANITIZE_STRING);
+    $gameCoverUrl = filter_var($gameCoverUrl, FILTER_VALIDATE_URL);
+    $gameWebUrl = filter_var($gameWebUrl, FILTER_VALIDATE_URL);
     
     $stmt = $pdo->prepare("INSERT INTO JEUX (nom_jeux, description_jeux, editeur_jeux, date_sortie_jeux, couverture_url_jeux, site_url_jeux) 
                             VALUES (:gameName, :gameDesc, :gameEditor, :gameRelease, :gameCoverUrl, :gameWebUrl)");
@@ -29,7 +83,26 @@ function addGame($pdo, $gameName, $gameDesc, $gameEditor, $gameRelease, $gameCov
     
     $stmt->execute();
 
-  
+    $gameId = $pdo->lastInsertId();
+
+    // Insérer les plateformes dans la table appartenir
+    foreach ($platforms as $platform) {
+   
+        $platform = filter_var($platform, FILTER_SANITIZE_STRING);
+
+        // Recherche de l'ID de la plateforme
+        $platformStmt = $pdo->prepare("SELECT Id_plateforme FROM PLATEFORME WHERE nom_plateforme = :platform");
+        $platformStmt->execute([':platform' => $platform]);
+        $platformRow = $platformStmt->fetch(PDO::FETCH_ASSOC);
+        
+        if ($platformRow) {
+            $platformId = $platformRow['Id_plateforme'];
+            
+            // Insertion dans la table appartenir
+            $appartenirStmt = $pdo->prepare("INSERT INTO appartenir (Id_jeux, Id_plateforme, Num_odre_plateforme) VALUES (:gameId, :platformId, 1)"); 
+            $appartenirStmt->execute([':gameId' => $gameId, ':platformId' => $platformId]);
+        }
+    }
 }
 
 /**
--- view/gameAdd.php
+++ view/gameAdd.php
@@ -1,101 +1,61 @@
-<?php
-// Ici, vous pouvez gérer la logique de traitement du formulaire si nécessaire.
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type: text/html" content="charset=utf-8">
+    <meta http-equiv="X-Content-Type-Options" content="nosniff">
     <title>Ajouter un jeu à la bibliothèque</title>
     <link href="styles/gameAdd.css" rel="stylesheet" />
 </head>
 <body>
     <header>
-        <!-- Votre logo et navigation ici -->
+        <!-- Ajouter header -->
     </header>
 
     <main>
         <h1>Ajouter un jeu à sa bibliothèque</h1>
 
         
-        
+        <!-- Récupère les infos pour ajouter un jeu à la BDD -->
         <form id="game-add-form" method="post">
-        <?php
 
-            // Include your database connection here
-            $host = 'localhost';
-            $db = 'jeux'; // This is your database name
-            $user = 'root';
-            $pass = '';
-            $charset = 'utf8mb4';
-
-            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-            $options = [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES => false,
-            ];
-
-            try {
-                $pdo = new PDO($dsn, $user, $pass, $options);
-            } catch (\PDOException $e) {
-                throw new \PDOException($e->getMessage(), (int)$e->getCode());
-            }
-
-            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_game'])) {
-    
-                // Assurez-vous que les clés de $_POST correspondent aux attributs "name" des champs de formulaire
-                $gameName = $_POST['game_name'] ?? ''; // Utilisez l'opérateur de coalescence nulle pour éviter les avertissements
-                $gameDesc = $_POST['game_desc'] ?? '';
-                $gameEditor = $_POST['game_editor'] ?? '';
-                $gameRelease = $_POST['game_release'] ?? '';
-                $gameCoverUrl = $_POST['game_cover_url'] ?? '';
-                $gameWebUrl = $_POST['game_web_url'] ?? '';
-                $platforms = $_POST['platforms'] ?? []; // Pour les cases à cocher, cela garantit que vous obtenez un tableau, même s'il est vide
-                
-                // Appeler votre fonction pour ajouter le jeu
-                addGame($pdo, $gameName, $gameDesc, $gameEditor, $gameRelease, $gameCoverUrl, $gameWebUrl, $platforms);
-            }
-            
-
-
-            ?>
+            <!-- Mise en place du jeton CSRF -->
+            <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token'], ENT_QUOTES, 'UTF-8'); ?>">
 
 
             <div class="form-group">
                 <label for="game-name">Nom du jeu</label>
-                <input type="text" id="game-name" name="game_name" required>
+                <input type="text" id="game-name" name="game_name" value="<?php echo isset($_POST['game_name']) ? htmlspecialchars($_POST['game_name'], ENT_QUOTES, 'UTF-8') : ''; ?>" required>
             </div>
             
             <div class="form-group">
                 <label for="game-editor">Editeur du jeu</label>
-                <input type="text" id="game-editor" name="game_editor" required>
+                <input type="text" id="game-editor" name="game_editor" value="<?php echo isset($_POST['game_editor']) ? htmlspecialchars($_POST['game_editor'], ENT_QUOTES, 'UTF-8') : ''; ?>" required>
             </div>
             
             <div class="form-group">
                 <label for="game-release">Sortie du jeu</label>
-                <input type="date" id="game-release" name="game_release" required>
+                <input type="date" id="game-release" name="game_release" value="<?php echo isset($_POST['game_release']) ? htmlspecialchars($_POST['game_release'], ENT_QUOTES, 'UTF-8') : ''; ?>" required>
             </div>
             
             <div class="form-group">
                 <label>Plateformes</label>
                 <div class="checkbox-group">
                     <div>
-                        <input type="checkbox" id="playstation" name="platforms[]" value="Playstation">
+                        <input type="checkbox" id="playstation" name="platforms[]" value="Playstation" <?php echo isset($_POST['platforms']) && in_array('Playstation', $_POST['platforms']) ? 'checked' : ''; ?>>
                         <label for="playstation">Playstation</label>
                     </div>
                     <div>
-                        <input type="checkbox" id="xbox" name="platforms[]" value="Xbox">
+                        <input type="checkbox" id="xbox" name="platforms[]" value="Xbox" <?php echo isset($_POST['platforms']) && in_array('Xbox', $_POST['platforms']) ? 'checked' : ''; ?>>
                         <label for="xbox">Xbox</label>
                     </div>
                     <div>
-                        <input type="checkbox" id="nintendo" name="platforms[]" value="Nintendo">
+                        <input type="checkbox" id="nintendo" name="platforms[]" value="Nintendo" <?php echo isset($_POST['platforms']) && in_array('Nintendo', $_POST['platforms']) ? 'checked' : ''; ?>>
                         <label for="nintendo">Nintendo</label>
                     </div>
                     <div>
-                        <input type="checkbox" id="pc" name="platforms[]" value="PC">
+                        <input type="checkbox" id="pc" name="platforms[]" value="PC" <?php echo isset($_POST['platforms']) && in_array('PC', $_POST['platforms']) ? 'checked' : ''; ?>>
                         <label for="pc">PC</label>
                     </div>
                 </div>
@@ -104,17 +64,17 @@
             
             <div class="form-group">
                 <label for="game-description">Description du jeu</label>
-                <textarea id="game-description" name="game_desc" required></textarea>
+                <textarea id="game-description" name="game_desc"><?php echo isset($_POST['game_desc']) ? htmlspecialchars($_POST['game_desc'], ENT_QUOTES, 'UTF-8') : ''; ?></textarea>
             </div>
             
             <div class="form-group">
                 <label for="cover-url">URL de la cover</label>
-                <input type="url" id="cover-url" name="game_cover_url" required>
+                <input type="url" id="cover-url" name="game_cover_url" value="<?php echo isset($_POST['game_cover_url']) ? htmlspecialchars($_POST['game_cover_url'], ENT_QUOTES, 'UTF-8') : ''; ?>">
             </div>
             
             <div class="form-group">
                 <label for="site-url">URL du site</label>
-                <input type="url" id="site-url" name="game_web_url" required>
+                <input type="url" id="site-url" name="game_web_url" value="<?php echo isset($_POST['game_web_url']) ? htmlspecialchars($_POST['game_web_url'], ENT_QUOTES, 'UTF-8') : ''; ?>">
             </div>
             
             
@@ -124,6 +84,7 @@
     </main>
 
     <footer>
+        <!-- Ajouter footer à la place -->
         Game Collection - 2023 - Tous droits réservés
     </footer>
 </body>
