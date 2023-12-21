be2c0b0153596eee6e5fca184aa37084f57e2ca5
Vigouroux Johanne <102874093+johannvig@users.noreply.github.com>
2023-12-21 21:34:19 +0100

    mise en forme propre du foreach de gameList.php

--- view/gameList.php
+++ view/gameList.php
@@ -34,20 +34,22 @@
         <h2>Mes jeux</h2>
         <div class="game-container">
         
-        <?php
-            
-            foreach ($games as $game) {
-                echo "<form method='post' class='game-form'>";
-                echo "<input type='hidden' name='game_id' value='" . htmlspecialchars($game['Id_jeux']) . "'>";
-                echo "<div class='game' style='background-image: url(\"" . htmlspecialchars($game['couverture_url_jeux']) . "\");'>";
-                echo "<div class='game-overlay'></div>";
-                echo "<h3>" . htmlspecialchars($game['nom_jeux']) . "</h3>";
-                echo "<p>" . htmlspecialchars($game['editeur_jeux']) . "</p>";
-                echo "<button type='submit' name='add_collection'>AJOUTER À LA BIBLIOTHÈQUE</button>";
-                echo "</div>";
-                echo "</form>";
-            }
+        <?php            
+        foreach ($games as $game) {
             ?>
+            <form method='post' class='game-form'>
+                <input type='hidden' name='game_id' value='<?php echo htmlspecialchars($game['Id_jeux']); ?>'>
+                <div class='game' style='background-image: url("<?php echo htmlspecialchars($game['couverture_url_jeux']); ?>");'>
+                    <div class='game-overlay'></div>
+                    <h3><?php echo htmlspecialchars($game['nom_jeux']); ?></h3>
+                    <p><?php echo htmlspecialchars($game['editeur_jeux']); ?></p>
+                    <button type='submit' name='add_collection'>AJOUTER À LA BIBLIOTHÈQUE</button>
+                </div>
+            </form>
+            <?php
+        }
+        ?>
+
 
         </div>
     </section>
