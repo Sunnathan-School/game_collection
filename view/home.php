246c788b8bb4f0c691cccd3f03c5cf9066c737ca
Amélien DENIS <ameliendenis72@gmail.com>
2023-12-22 16:33:12 +0100

    update home page view

--- view/home.php
+++ view/home.php
@@ -7,14 +7,93 @@
     <title>Accueil</title>
 </head>
 <body>
-    <div class="home_header">
-        <div class="head_text">
-            <h2>Salut {USERNAME} !</h2>
-            <h2>Prêt à ajouter des jeux à ta collection ?</h2>
-        </div>
-        <img src="assets/img/zelda_home.jpeg" alt="" class="head_img">
-        
 
+    <header>
+        <div class="logo">Logo</div>
+        <div class="acces">
+            <a href="#">MA BIBLIOTHÈQUE</a>
+            <a href="#">AJOUTER UN JEU</a>
+            <a href="#">CLASSEMENT</a>
+            <a href="#">PROFIL</a>
+        </div>
+    </header>
+
+    <div class="bandeauAccueil">
+        <div class="textBandeau">
+            <h2>SALUT {USERNAME} !</h2>
+            <h2>PRÊT À AJOUTER DES</h2>
+            <h2>JEUX À TA COLLECTION ?</h2>
+        </div>
     </div>
+
+    <section id="game-library">
+        <h2>Mes jeux</h2>
+        <div class="list">
+            <!-- ajouter le for each -->
+            <div class='game' style='background-image: url("assets/img/exempleJeu.jpg");'>
+                <div class='game-overlay'>
+                    <div class="text">
+                        <h3>Red Dead Redemption</h3>
+                        <p>Playstation</p>
+                    </div>
+                    <p id="temps">8H</p>
+                </div>
+            </div>
+
+            <div class='game' style='background-image: url("assets/img/exempleJeu.jpg");'>
+                <div class='game-overlay'>
+                    <div class="text">
+                        <h3>Un jeu d'avaneture connu avec un nom plutôt long</h3>
+                        <p>Playstation</p>
+                    </div>
+                    <p id="temps">8H</p>
+                </div>
+            </div>
+
+            <div class='game' style='background-image: url("assets/img/exempleJeu.jpg");'>
+                <div class='game-overlay'>
+                    <div class="text">
+                        <h3>Red Dead Redemption</h3>
+                        <p>Playstation</p>
+                    </div>
+                    <p id="temps">8H</p>
+                </div>
+            </div>
+
+            <div class='game' style='background-image: url("assets/img/exempleJeu.jpg");'>
+                <div class='game-overlay'>
+                    <div class="text">
+                        <h3>Red Dead Redemption</h3>
+                        <p>Playstation</p>
+                    </div>
+                    <p id="temps">8H</p>
+                </div>
+            </div>
+
+            <div class='game' style='background-image: url("assets/img/exempleJeu.jpg");'>
+                <div class='game-overlay'>
+                    <div class="text">
+                        <h3>Red Dead Redemption</h3>
+                        <p>Playstation</p>
+                    </div>
+                    <p id="temps">8H</p>
+                </div>
+            </div>
+
+            <div class='game' style='background-image: url("assets/img/exempleJeu.jpg");'>
+                <div class='game-overlay'>
+                    <div class="text">
+                        <h3>Red Dead Redemption</h3>
+                        <p>Playstation</p>
+                    </div>
+                    <p id="temps">8H</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <footer></footer>
+
 </body>
 </html>
