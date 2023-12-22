436b4cd5f27b598c43a45e030f9b5bd12f18d962
Vigouroux Johanne <102874093+johannvig@users.noreply.github.com>
2023-12-22 14:01:09 +0100

    update gameAdd.php
    -modification du css pour correspondre à la maquette

--- view/gameAdd.php
+++ view/gameAdd.php
@@ -19,21 +19,17 @@
         <h1>Ajouter un jeu à sa bibliothèque</h1>
 
         
-        <!-- Récupère les infos pour ajouter un jeu à la BDD -->
         <form id="game-add-form" method="post">
-
-            <!-- Mise en place du jeton CSRF -->
-            <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token'], ENT_QUOTES, 'UTF-8'); ?>">
-
+            <p>Le jeu que vous souhaiter ajouter n'existe pas ! Vous pouvez le créer, celui-ci sera automatiquement ajouter a votre bibliothèque !</p>
 
             <div class="form-group">
                 <label for="game-name">Nom du jeu</label>
-                <input type="text" id="game-name" name="game_name" value="<?php echo isset($_POST['game_name']) ? htmlspecialchars($_POST['game_name'], ENT_QUOTES, 'UTF-8') : ''; ?>" required>
+                <input type="text" id="game-name" name="game_name" placeholder="Nom du jeu" value="<?php echo isset($_POST['game_name']) ? htmlspecialchars($_POST['game_name'], ENT_QUOTES, 'UTF-8') : ''; ?>" required>
             </div>
             
             <div class="form-group">
                 <label for="game-editor">Editeur du jeu</label>
-                <input type="text" id="game-editor" name="game_editor" value="<?php echo isset($_POST['game_editor']) ? htmlspecialchars($_POST['game_editor'], ENT_QUOTES, 'UTF-8') : ''; ?>" required>
+                <input type="text" id="game-editor" name="game_editor" placeholder="Editeur du jeu" value="<?php echo isset($_POST['game_editor']) ? htmlspecialchars($_POST['game_editor'], ENT_QUOTES, 'UTF-8') : ''; ?>" required>
             </div>
             
             <div class="form-group">
@@ -45,20 +41,20 @@
                 <label>Plateformes</label>
                 <div class="checkbox-group">
                     <div>
-                        <input type="checkbox" id="playstation" name="platforms[]" value="Playstation" <?php echo isset($_POST['platforms']) && in_array('Playstation', $_POST['platforms']) ? 'checked' : ''; ?>>
                         <label for="playstation">Playstation</label>
+                        <input type="checkbox" id="playstation" name="platforms[]" value="Playstation" <?php echo isset($_POST['platforms']) && in_array('Playstation', $_POST['platforms']) ? 'checked' : ''; ?>>
                     </div>
                     <div>
-                        <input type="checkbox" id="xbox" name="platforms[]" value="Xbox" <?php echo isset($_POST['platforms']) && in_array('Xbox', $_POST['platforms']) ? 'checked' : ''; ?>>
                         <label for="xbox">Xbox</label>
+                        <input type="checkbox" id="xbox" name="platforms[]" value="Xbox" <?php echo isset($_POST['platforms']) && in_array('Xbox', $_POST['platforms']) ? 'checked' : ''; ?>> 
                     </div>
                     <div>
-                        <input type="checkbox" id="nintendo" name="platforms[]" value="Nintendo" <?php echo isset($_POST['platforms']) && in_array('Nintendo', $_POST['platforms']) ? 'checked' : ''; ?>>
                         <label for="nintendo">Nintendo</label>
+                        <input type="checkbox" id="nintendo" name="platforms[]" value="Nintendo" <?php echo isset($_POST['platforms']) && in_array('Nintendo', $_POST['platforms']) ? 'checked' : ''; ?>>
                     </div>
                     <div>
-                        <input type="checkbox" id="pc" name="platforms[]" value="PC" <?php echo isset($_POST['platforms']) && in_array('PC', $_POST['platforms']) ? 'checked' : ''; ?>>
                         <label for="pc">PC</label>
+                        <input type="checkbox" id="pc" name="platforms[]" value="PC" <?php echo isset($_POST['platforms']) && in_array('PC', $_POST['platforms']) ? 'checked' : ''; ?>>
                     </div>
                 </div>
             </div>
@@ -66,17 +62,17 @@
             
             <div class="form-group">
                 <label for="game-description">Description du jeu</label>
-                <textarea id="game-description" name="game_desc"><?php echo isset($_POST['game_desc']) ? htmlspecialchars($_POST['game_desc'], ENT_QUOTES, 'UTF-8') : ''; ?></textarea>
+                <textarea id="game-description" name="game_desc" placeholder="Description du jeu"><?php echo isset($_POST['game_desc']) ? htmlspecialchars($_POST['game_desc'], ENT_QUOTES, 'UTF-8') : ''; ?></textarea>
             </div>
             
             <div class="form-group">
                 <label for="cover-url">URL de la cover</label>
-                <input type="url" id="cover-url" name="game_cover_url" value="<?php echo isset($_POST['game_cover_url']) ? htmlspecialchars($_POST['game_cover_url'], ENT_QUOTES, 'UTF-8') : ''; ?>">
+                <input type="url" id="cover-url" name="game_cover_url" placeholder="URL de la cover" value="<?php echo isset($_POST['game_cover_url']) ? htmlspecialchars($_POST['game_cover_url'], ENT_QUOTES, 'UTF-8') : ''; ?>">
             </div>
             
             <div class="form-group">
                 <label for="site-url">URL du site</label>
-                <input type="url" id="site-url" name="game_web_url" value="<?php echo isset($_POST['game_web_url']) ? htmlspecialchars($_POST['game_web_url'], ENT_QUOTES, 'UTF-8') : ''; ?>">
+                <input type="url" id="site-url" name="game_web_url" placeholder="URL du site" value="<?php echo isset($_POST['game_web_url']) ? htmlspecialchars($_POST['game_web_url'], ENT_QUOTES, 'UTF-8') : ''; ?>">
             </div>
             
             
